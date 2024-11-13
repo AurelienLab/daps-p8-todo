541c51930d5112dbdd82ba85da758719c120b683
Aurélien Lorence <aurelien@keyframe.fr>
2024-11-13 11:34:00 +0100

    Security basic configuration #1

---
+++ src/Controller/SecurityController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Attribute\Route;
+class SecurityController extends AbstractController
+{
+    #[Route('/login', name: 'login')]
+    public function loginAction(Request $request)
+    {
+        $authenticationUtils = $this->get('security.authentication_utils');
+
+        $error = $authenticationUtils->getLastAuthenticationError();
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        return $this->render('security/login.html.twig', array(
+            'last_username' => $lastUsername,
+            'error'         => $error,
+        ));
+    }
+}
